9d353923543c7c18ecf79826c20672d390afa5f9
JongMinCh0i <wonderland314592@gmail.com>
2024-01-22 23:08:07 +0900

    test

--- src/main/java/jpabook/JpaMain.java
+++ src/main/java/jpabook/JpaMain.java
@@ -6,7 +6,7 @@ import javax.persistence.EntityTransaction;
 import javax.persistence.Persistence;
 
 
-// test for gitHub
+// test for gitHub123
 public class JpaMain {
     public static void main(String[] args) {
         EntityManagerFactory emf = Persistence.createEntityManagerFactory("hello");
